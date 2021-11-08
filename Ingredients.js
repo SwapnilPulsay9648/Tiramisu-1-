f1de08c1c63cd2f31e05705d8f3bae3ff29e3e2e
SwapnilPulsay9648 <88311829+SwapnilPulsay9648@users.noreply.github.com>
2021-11-08 16:52:32 +0530

    Add files via upload

---
+++ Ingredients.js
@@ -0,0 +1,9 @@
+1 cup all purpose flour (maida) or 105 grams all purpose flour
+▢½ teaspoon baking powder
+▢1 light pinch salt – OPTIONAL and only if using unsalted butter
+▢½ cup sweetened condensed milk or 125 ml
+3 tablespoons sugar
+½ cup water or 125 ml water
+▢¼ cup Butter or 40 grams butter – salted or unsalted
+▢1 teaspoon vanilla extract or ½ teaspoon vanilla essence
+▢1 to 2 teaspoon cocoa powder for dusting
---
+++ Recipe.js
@@ -0,0 +1,9 @@
+1. In an eggless tiramisu recipe, the tiramisu filling is made by making a smooth mixture of whipped mascarpone cheese, whipped cream, powdered sugar and vanilla extract.
+
+2. The sponge fingers or sponge cake slices are dipped in coffee syrup. These are then layered in a glass pan or bowl. Now the mascarpone cheese+whipped cream filling mixture is spread evenly on the sponge fingers. 
+
+3. In a similar way 1 to 2 more layers of the coffee syrup soaked sponge fingers and mascarpone cheese+whipped cream are made. 
+4. The top layer is of the mascarpone cheese+whipped cream which is dusted with some cocoa powder. 
+
+5. The tiramisu is ready now and has to be refrigerated for 4 to 5 hours or overnight for it to set. After the tiramisu is set, you can slice it and serve as a dessert. 
+
